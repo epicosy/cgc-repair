182b8cfadf88f9b20817753a6e15947b1c8d9c92
epicosy <xtremedy@gmail.com>
2021-07-06 17:20:42 +0100

    install_cgc_lib with execute permissions

--- scripts/install_cgc_lib.sh
+++ scripts/install_cgc_lib.sh
@@ -25,17 +25,17 @@ echo "cgc-repair include and 32 bit libraries installed"
 
 platform_arch=$(uname -i)
 if [ "$platform_arch" == "x86_64" ]; then
-        mkdir -p "/usr/local/lib64/cgc"
-	mkdir -p "${build_dir}64"
-	unset M32
-	cd "${build_dir}64" && cmake "$cmake_opts" "-DCMAKE_SYSTEM_PROCESSOR=amd64" "$include_src_dir"
-	[[ $? -eq 1 ]] && echo "[Error] cmake 64bit config failed" && exit 1 ;
+    mkdir -p "/usr/local/lib64/cgc"
+    mkdir -p "${build_dir}64"
+    unset M32
+    cd "${build_dir}64" && cmake "$cmake_opts" "-DCMAKE_SYSTEM_PROCESSOR=amd64" "$include_src_dir"
+    [[ $? -eq 1 ]] && echo "[Error] cmake 64bit config failed" && exit 1 ;
 
-	cmake --build .
-	[[ $? -eq 1 ]] && echo "[Error] cmake 64bit build failed" && exit 1 ;
+    cmake --build .
+    [[ $? -eq 1 ]] && echo "[Error] cmake 64bit build failed" && exit 1 ;
 
-	cmake --build . --target install
-	[[ $? -eq 1 ]] && echo "[Error] cmake 64 bit install failed" && exit 1 ;
+    cmake --build . --target install
+    [[ $? -eq 1 ]] && echo "[Error] cmake 64 bit install failed" && exit 1 ;
 
-	echo "cgc-repair 64 bit libraries installed"
+    echo "cgc-repair 64 bit libraries installed"
 fi
